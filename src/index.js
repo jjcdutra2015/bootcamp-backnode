060ac4d2e60707d5cc79f66ea57b50e7275c1f9a
Julio Dutra <jjcdutra@gmail.com>
2020-04-08 20:23:50 -0300

    Tipos de parametros

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/projects', (req, res) => {
+  const { title, owner } = req.query;
+
+  console.log(title);
+  console.log(owner);
+
   return res.json([
     'Project1',
     'Project2',
-    'Project3',
   ]);
 });
 
 app.post('/projects', (req, res) => {
+  const { title, owner } = req.body;
+
+  console.log(title);
+  console.log(owner);
+
   return res.json([
     'Project1',
     'Project2',
     'Project3',
-    'Project4',
   ]);
 });
 
 app.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+
   return res.json([
     'Project4',
     'Project2',
