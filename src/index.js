ff4c2cf013e065a7c985da209f7021315cf8d9a6
Julio Dutra <jjcdutra@gmail.com>
2020-04-08 17:04:52 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+app.get('/projects', (req, res) => {
+  return res.json([
+    'Project1',
+    'Project2',
+    'Project3',
+  ]);
+});
+
+app.post('/projects', (req, res) => {
+  return res.json([
+    'Project1',
+    'Project2',
+    'Project3',
+    'Project4',
+  ]);
+});
+
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    'Project4',
+    'Project2',
+    'Project3',
+  ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Project2',
+    'Project3',
+  ]);
 });
 
 app.listen(3333, () => {
